b717683a60ae7d832479e8c02cfe731f0d3e9b91
rezaelahidev <rezaelahi.dev@gmail.com>
2024-11-28 10:54:29 +0330

    Create the plugin main file

---
+++ blockera-site-toolkit.php
@@ -0,0 +1,56 @@
+<?php
+/*
+Plugin Name: Blockera Site Toolkit
+Description: A site toolkit plugin for Blockera AI.
+Version: 1.0
+Author: blockera.ai
+Tested up to: 6.7
+Domain Path: /languages
+Domain: blockera-site-toolkit
+*/
+
+// Exit if accessed directly.
+if (!defined('ABSPATH')) {
+    die('Access Denied!');
+}
+
+require __DIR__ . '/vendor/autoload.php';
+
+use BlockeraAI\SiteToolkit\Setup;
+use BlockeraAI\SiteToolkit\Commands\Database;
+use BlockeraAI\SiteToolkit\Database\Migrations;
+
+define('BSA_PLUGIN_FILE', __FILE__);
+define('BSA_PLUGIN_URL', plugin_dir_url(BSA_PLUGIN_FILE));
+define('BSA_PLUGIN_DIR', plugin_dir_path(BSA_PLUGIN_FILE));
+
+$migrations = [
+    new Migrations\ClientsTable(),
+    new Migrations\AccessTokenTable(),
+    new Migrations\RefreshTokenTable(),
+];
+
+/**
+ * @var Setup $setup
+ */
+$setup = Setup::getInstance();
+
+$setup->setMigrations($migrations)->mount()->unmount();
+
+/**
+ * Initialize the plugin.
+ *
+ * @since 1.0.0
+ */
+add_action('plugins_loaded', function () use ($setup): void {
+    add_action('init', static function () use ($setup): void {
+        $setup->bootstrap();
+    });
+});
+
+// Initialize the database commands.
+if (defined('WP_DEBUG') && WP_DEBUG && class_exists(WP_CLI::class)) {
+    $db = new Database();
+    $db->migrate($migrations);
+    $db->fresh($migrations);
+}
