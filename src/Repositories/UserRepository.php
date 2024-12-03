706110ef8dd814dfb1779bfdb10c63c20d853767
rezaelahidev <rezaelahi.dev@gmail.com>
2024-12-03 11:42:03 +0330

    feat: add new relation in UserRpository

--- src/Repositories/UserRepository.php
+++ src/Repositories/UserRepository.php
@@ -3,8 +3,8 @@
 namespace BlockeraAI\SiteToolkit\Repositories;
 
 use BlockeraAI\SiteToolkit\Entities\UserEntity;
-use League\OAuth2\Server\Entities\ClientEntityInterface;
 use League\OAuth2\Server\Entities\UserEntityInterface;
+use League\OAuth2\Server\Entities\ClientEntityInterface;
 
 class UserRepository implements \League\OAuth2\Server\Repositories\UserRepositoryInterface
 {
@@ -23,5 +23,31 @@ class UserRepository implements \League\OAuth2\Server\Repositories\UserRepositor
         }
 
         return null;
+    }
+
+    /**
+     * Get the clients of the user.
+     *
+     * @param integer $user_id The ID of the user.
+     * @return array The clients of the user.
+     */
+    public function getClients(int $user_id): array
+    {
+        global $wpdb;
+
+        $clients_table = $wpdb->prefix . 'auth_clients';
+        $licenses_table = $wpdb->prefix . 'auth_licenses';
+        $clients = $wpdb->get_results(
+            $wpdb->prepare(
+                "SELECT c.*, l.subscription_id as subscription_id 
+                FROM $clients_table c
+                LEFT JOIN $licenses_table l ON l.client_id = c.client_id
+                WHERE c.user_id = %d",
+                $user_id
+            ),
+            ARRAY_A
+        );
+
+        return $clients;
     }
 }
