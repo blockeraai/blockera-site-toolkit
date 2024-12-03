07ed30ba611840affdfeb07c42ef91085997bd6b
rezaelahidev <rezaelahi.dev@gmail.com>
2024-12-03 11:41:28 +0330

    refactor: views

--- src/Views/license-manager/shared-inputs-consent-form.php
+++ src/Views/license-manager/shared-inputs-consent-form.php
@@ -1,9 +1,13 @@
 <input type="hidden" name="scopes" value="update">
 <input type="hidden" name="_wpnonce" value="<?php echo wp_create_nonce('wp_rest'); ?>">
-<input type="hidden" name="client_id" value="<?php echo $_GET['client_id'] ?? '' ?>">
+<?php if (!isset($withoutClientId) || !$withoutClientId): ?>
+    <input type="hidden" name="client_id" value="<?php echo $_GET['client_id'] ?? '' ?>">
+<?php endif; ?>
 <input type="hidden" name="state" value="<?php echo $_GET['state'] ?? '' ?>">
 <input type="hidden" name="response_type" value="<?php echo $_GET['response_type'] ?? '' ?>">
 <input type="hidden" name="approval_prompt" value="<?php echo $_GET['approval_prompt'] ?? '' ?>">
 <input type="hidden" name="redirect_uri" value="<?php echo $_GET['redirect_uri'] ?? '' ?>">
-<button class="button button-primary" type="submit" name="action" value="allow"><?php _e('Allow', 'blockera-site-toolkit') ?></button>
-<button class="button button-secondary" type="submit" name="action" value="cancel"><?php _e('Cancel', 'blockera-site-toolkit'); ?></button>
+<?php if (!isset($withoutActions)): ?>
+    <button class="button button-primary" type="submit" name="action" value="allow"><?php _e('Allow', 'blockera-site-toolkit') ?></button>
+    <button class="button button-secondary" type="submit" name="action" value="cancel"><?php _e('Cancel', 'blockera-site-toolkit'); ?></button>
+<?php endif; ?>
