6d71c676d470c52b28f8c88ed21d9c9732652720
rezaelahidev <rezaelahi.dev@gmail.com>
2025-04-15 11:08:08 +0330

    feat: implemented securly downloadable files for each licenses

--- packages/site-toolkit/php/Routes/api.php
+++ packages/site-toolkit/php/Routes/api.php
@@ -1,4 +1,5 @@
 <?php
 
+include(__DIR__ . '/api/files.php');
 include(__DIR__ . '/api/product.php');
 include(__DIR__ . '/api/license-manager.php');
---
+++ packages/site-toolkit/php/Routes/api/files.php
@@ -0,0 +1,11 @@
+<?php
+
+use BlockeraAI\SiteToolkit\Http\Controller\FileController;
+
+$fileController = new FileController($this);
+
+register_rest_route('auth/v1', '/download', [
+	'methods' => 'POST',
+	'callback' => [$fileController, 'download'],
+	'permission_callback' => [$fileController, 'permission'],
+]);
