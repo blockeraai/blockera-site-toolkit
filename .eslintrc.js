9ef812e636a618513070ed3ec2a5de8ddf790f4e
rezaelahidev <rezaelahi.dev@gmail.com>
2026-08-14 12:55:15 +0330

    refactor: consume shared GitHub toolkit CI like other consumers
    Thin-wrap workflows onto global-packages/dev-tools actions and scripts,
    fix text domains for blockera-site-toolkit, add wp-env/base and
    bundle-size paths, and keep packages/site-toolkit covered in lint/test/CI.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,14 @@
-module.exports = require('./packages/global-packages/packages/dev-tools/js/eslint/config');
+const base = require('./packages/global-packages/packages/dev-tools/js/eslint/config');
+
+module.exports = {
+	...base,
+	rules: {
+		...base.rules,
+		'@wordpress/i18n-text-domain': [
+			'error',
+			{
+				allowedTextDomain: ['blockera', 'blockera-site-toolkit'],
+			},
+		],
+	},
+};
