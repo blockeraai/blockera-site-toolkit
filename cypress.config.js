d1e54c7d1ed4a1a93e3ec418249439099faac6e8
rezaelahidev <rezaelahi.dev@gmail.com>
2026-08-14 12:55:15 +0330

    refactor: consume shared GitHub toolkit CI like other consumers
    Thin-wrap workflows onto global-packages/dev-tools actions and scripts,
    fix text domains for blockera-site-toolkit, add wp-env/base and
    bundle-size paths, and keep packages/site-toolkit covered in lint/test/CI.

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 module.exports =
 	require('./packages/global-packages/packages/dev-tools/js/cypress/config')({
 		rootDir: __dirname,
-		projectId: 'blockera',
+		projectId: 'blockera-site-toolkit',
 		e2eSpecPattern: ['packages/**/*.toolkit.e2e.cy.js'],
 		e2eExcludeSpecPattern: [],
 		alwaysExcludeSpecPattern: ['packages/**/*.build.e2e.js'],
