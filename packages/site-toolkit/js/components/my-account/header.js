effdb1da64c439697fba7e0fa56e148219e4db75
rezaelahidev <rezaelahi.dev@gmail.com>
2026-08-15 22:43:55 +0330

    style(js-lint): reformat codes

--- packages/site-toolkit/js/components/my-account/header.js
+++ packages/site-toolkit/js/components/my-account/header.js
@@ -39,7 +39,7 @@ export const Header = ({
 
 	const remainingDays = Math.ceil(
 		(new Date(expiryDate).getTime() - new Date().getTime()) /
-		(1000 * 60 * 60 * 24)
+			(1000 * 60 * 60 * 24)
 	);
 
 	return (
