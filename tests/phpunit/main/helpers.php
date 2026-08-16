41fa163e52d147bcfb1c6c05c89e99b75e983451
rezaelahidev <rezaelahi.dev@gmail.com>
2026-08-16 18:27:43 +0330

    test: align PHPUnit units/snapshots setup with blockera

---
+++ tests/phpunit/main/helpers.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Shared PHPUnit helpers for blockera-site-toolkit.
+ *
+ * Keep this file for composer autoload-dev parity with the blockera plugin.
+ * Add consumer-specific helpers here as snapshot/integration tests grow.
+ *
+ * @package blockera-site-toolkit
+ */
+
