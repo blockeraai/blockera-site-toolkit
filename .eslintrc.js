db5088561152a99b71bf4a2f4c4e4d1b90c24b39
Ali Aghdam <betteraliaghdam@gmail.com>
2026-08-18 20:44:09 +0330

    chore: sync project configuration files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,5 @@
+// Edit packages/global-packages/packages/dev-tools/root-configs/.eslintrc.blockera-site-toolkit.js
+// project:bootstrap copies this to the host repo root for --project=blockera-site-toolkit.
 const base = require('./packages/global-packages/packages/dev-tools/js/eslint/config');
 
 module.exports = {
