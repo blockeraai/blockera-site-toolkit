3b2aa677bee0c5ba5a71524d0699a64d36ed1afa
Ali Aghdam <betteraliaghdam@gmail.com>
2026-09-16 14:19:44 +0330

    chore: code format

--- jest.config.js
+++ jest.config.js
@@ -4,17 +4,14 @@
  * Site-toolkit Jest: only the site-toolkit package under global-packages.
  * Shared package unit tests run in blockera-global-packages / other product consumers.
  */
-const path = require( 'path' );
+const path = require('path');
 
-const base = require( './packages/global-packages/packages/dev-jest/js/jest.config.js' );
+const base = require('./packages/global-packages/packages/dev-jest/js/jest.config.js');
 
 module.exports = {
 	...base,
 	roots: [
-		path.join(
-			__dirname,
-			'packages/global-packages/packages/site-toolkit'
-		),
+		path.join(__dirname, 'packages/global-packages/packages/site-toolkit'),
 	],
 	collectCoverageFrom: [
 		'<rootDir>/packages/global-packages/packages/site-toolkit/**/*.js',
